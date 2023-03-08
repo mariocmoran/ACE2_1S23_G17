cbaeed36998da36d0a26b3bd5c6b46a6a400888c
Mario <maritocesar987@gmail.com>
2023-03-08 00:15:18 -0600

    inicio proyecto 1

---
+++ Proyecto 1/arduino/proyecto/proyecto.ino
@@ -0,0 +1,19 @@
+int pomodoroSecs[8] = {
+/* POMODOROS */
+/* primero  |  segundo |  tercero | cuarto   */
+	1500, 300, 1500, 300, 1500, 300, 1500, 900 
+}; 
+bool isWorkState[8] = { 
+	true, false, true, false, true, false, true, false 
+};  
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
