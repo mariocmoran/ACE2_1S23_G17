2ad1ed8dc23deed831bf334b0517ea2cac2f6cc5
Mario <maritocesar987@gmail.com>
2023-03-21 01:00:31 -0600

    fases pomodoro + alarmas + ajuste tiempo total (dimmer)

--- Proyecto 1/arduino/proyecto/proyecto.ino
+++ Proyecto 1/arduino/proyecto/proyecto.ino
@@ -5,75 +5,110 @@ int speakerPin = 15; // PIN DEL BUZZER
 
 #define DIGIT_ON  LOW
 #define DIGIT_OFF  HIGH
-// NOTAS DE LA CANCION ----------------------------------------------------------------------------------------
-#define NOTE_C4  262   //Defining note frequency
-#define NOTE_D4  294
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_G4  392
-#define NOTE_A4  440
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_D5  587
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_G5  784
-#define NOTE_A5  880
-#define NOTE_B5  988
-//----------------------------------------------------------------------------------------
-int notes[] = {       //Note of the song, 0 is a rest/pulse
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
-   
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
-   
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
-   NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
-   
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
-   NOTE_C5, NOTE_A4, NOTE_B4, 0,
 
-   NOTE_A4, NOTE_A4, 
-   //Repeat of first part
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+// CANCION DE FINALIZACION --------------------------------------------------------------
+int tempo = 180;
+#define NOTE_B0  31
+  #define NOTE_C1  33
+  #define NOTE_CS1 35
+  #define NOTE_D1  37
+  #define NOTE_DS1 39
+  #define NOTE_E1  41
+  #define NOTE_F1  44
+  #define NOTE_FS1 46
+  #define NOTE_G1  49
+  #define NOTE_GS1 52
+  #define NOTE_A1  55
+  #define NOTE_AS1 58
+  #define NOTE_B1  62
+  #define NOTE_C2  65
+  #define NOTE_CS2 69
+  #define NOTE_D2  73
+  #define NOTE_DS2 78
+  #define NOTE_E2  82
+  #define NOTE_F2  87
+  #define NOTE_FS2 93
+  #define NOTE_G2  98
+  #define NOTE_GS2 104
+  #define NOTE_A2  110
+  #define NOTE_AS2 117
+  #define NOTE_B2  123
+  #define NOTE_C3  131
+  #define NOTE_CS3 139
+  #define NOTE_D3  147
+  #define NOTE_DS3 156
+  #define NOTE_E3  165
+  #define NOTE_F3  175
+  #define NOTE_FS3 185
+  #define NOTE_G3  196
+  #define NOTE_GS3 208
+  #define NOTE_A3  220
+  #define NOTE_AS3 233
+  #define NOTE_B3  247
+  #define NOTE_C4  262
+  #define NOTE_CS4 277
+  #define NOTE_D4  294
+  #define NOTE_DS4 311
+  #define NOTE_E4  330
+  #define NOTE_F4  349
+  #define NOTE_FS4 370
+  #define NOTE_G4  392
+  #define NOTE_GS4 415
+  #define NOTE_A4  440
+  #define NOTE_AS4 466
+  #define NOTE_B4  494
+  #define NOTE_C5  523
+  #define NOTE_CS5 554
+  #define NOTE_D5  587
+  #define NOTE_DS5 622
+  #define NOTE_E5  659
+  #define NOTE_F5  698
+  #define NOTE_FS5 740
+  #define NOTE_G5  784
+  #define NOTE_GS5 831
+  #define NOTE_A5  880
+  #define NOTE_AS5 932
+  #define NOTE_B5  988
+  #define NOTE_C6  1047
+  #define NOTE_CS6 1109
+  #define NOTE_D6  1175
+  #define NOTE_DS6 1245
+  #define NOTE_E6  1319
+  #define NOTE_F6  1397
+  #define NOTE_FS6 1480
+  #define NOTE_G6  1568
+  #define NOTE_GS6 1661
+  #define NOTE_A6  1760
+  #define NOTE_AS6 1865
+  #define NOTE_B6  1976
+  #define NOTE_C7  2093
+  #define NOTE_CS7 2217
+  #define NOTE_D7  2349
+  #define NOTE_DS7 2489
+  #define NOTE_E7  2637
+  #define NOTE_F7  2794
+  #define NOTE_FS7 2960
+  #define NOTE_G7  3136
+  #define NOTE_GS7 3322
+  #define NOTE_A7  3520
+  #define NOTE_AS7 3729
+  #define NOTE_B7  3951
+  #define NOTE_C8  4186
+  #define NOTE_CS8 4435
+  #define NOTE_D8  4699
+  #define NOTE_DS8 4978
+  #define REST      0
 
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
-   
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
-   NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
-   
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
-   NOTE_C5, NOTE_A4, NOTE_B4, 0,
-   //End of Repeat
-
-   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
-   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
-   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
-   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4,
-
-   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
-   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
-   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
-   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4
+int melody[] = {
+  NOTE_E5, 8, NOTE_D5, 8, NOTE_FS4, 4, NOTE_GS4, 4, 
+  NOTE_CS5, 8, NOTE_B4, 8, NOTE_D4, 4, NOTE_E4, 4, 
+  NOTE_B4, 8, NOTE_A4, 8, NOTE_CS4, 4, NOTE_E4, 4,
+  NOTE_A4, 2, 
 };
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+int wholenote = (60000 * 4) / tempo;
+int divider = 0, noteDuration = 0;
+
 //----------------------------------------------------------------------------------------
 // SEGMENTOS PARA EL DISPLAY DE 3 BITS
 int segA = 2; 
@@ -86,75 +121,61 @@ int segG = 8;
 //int segPD = ; 
 
 // ASIGNACION DE PIN DIGITAL A CADA BOTON (provisional)
-int button1=13; // RESETEAR E INICIAR EL DISPLAY
-int button2=12;
-int button3=16;
+int button1=13; // INICIAR EL DISPLAY
+int button2=12; // RESETEAR EL DISPLAY
+int button3=16; // DURACION TOTAL POMODORO
 int button4=17;
-
-const int buzzer = 24;
-const int songspeed = 1.5;
-
-int countdown_time = 90;
-int rest_time = 30;
-// DURACION DE LAS NOTAS DE LA CANCION -------------------------------------------------------------------
-int duration[] = {         //duration of each note (in ms) Quarter Note is set to 250 ms
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 125, 250, 125,
-
-  125, 125, 250, 125, 125, 
-  250, 125, 250, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 375, 375,
-
-  250, 125,
-  //Rpeat of First Part
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 125, 250, 125,
-
-  125, 125, 250, 125, 125, 
-  250, 125, 250, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 375, 375,
-  //End of Repeat
-  
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 125, 125, 125, 375,
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 500,
-
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 125, 125, 125, 375,
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 500
-};
+//----------------------------------------------------------------------------------------
+const int buzzer = 24;// ALARMA BUZZER
+int dimmer = A1; // LECTURA DEL DIMMER
+bool working = false;
+//----------------------------------------------------------------------------------------
+int countdown_time = 45; // TIEMPO DE TRABAJO DEFAULT
+int rest_time = 15; // TIEMPO DE DESCANSO DEFAULT
+int total_time = 1; // TIEMPO TOTAL DEL POMODORO EN MINUTOS
 //----------------------------------------------------------------------------------------
 struct struct_digits { // STRUCT DONDE SE DEFINEN LOS DIGITOS
   int digit[4] = {0,0,0,0};
 };
 
+void stopWorkingAlarm(){
+  tone(buzzer,2000,500); //tone(pin,frequency,duration)
+  delay(900);
+  tone(buzzer,2000,500); //tone(pin,frequency,duration)
+  delay(900);
+  tone(buzzer,2000,500); //tone(pin,frequency,duration)
+  delay(900);
+  tone(buzzer,2000,500); //tone(pin,frequency,duration)
+  delay(900);
+  tone(buzzer,2000,500); //tone(pin,frequency,duration)
+}
+
+void startWorkingAlarm(){
+  tone(buzzer,1500,2000); //tone(pin,frequency,duration)
+  delay(2000);
+  tone(buzzer,1500,2000); //tone(pin,frequency,duration)
+}
+
+void finishAlarm(){
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+    // stop the waveform generation before the next note.
+    noTone(buzzer);
+  }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -174,6 +195,7 @@ void setup() {
 
   //PIN DE SALIDA PARA EL BUZZER
   pinMode(speakerPin, OUTPUT);
+  //PIN ENTRADA ANALOGICA DEL DIMMER
 
   // PINES DE ENTARDA PARA LOS BOTONES
   pinMode(button1,INPUT_PULLUP);
@@ -182,19 +204,10 @@ void setup() {
   pinMode(button4,INPUT_PULLUP);
 }
 
-
-void playTone(int tone, int duration) { // ALARMA PARA CUANDO SE ACABA EL TIEMPO
-  for (long k = 0; k < duration * 1000L; k += tone * 2) {  
-    digitalWrite(speakerPin, HIGH);
-    delayMicroseconds(tone); 
-    digitalWrite(speakerPin, LOW);
-    delayMicroseconds(tone);
-  }
-}
-
+// ENCENDER LOS SEGMENTOS DEL DISPLAY
 void lightNumber(int numberToDisplay) { // FUNCION DE NUMERO A ENCENDER
-#define SEGMENT_ON  HIGH
-#define SEGMENT_OFF LOW
+  #define SEGMENT_ON  HIGH
+  #define SEGMENT_OFF LOW
 
   switch (numberToDisplay){ // OBTIENE EL NUMERO Y LO MUESTRA EN EL DISPLAY
   case 0:
@@ -348,17 +361,15 @@ struct struct_digits IntToDigits(int n){
     //Serial.print("minutos length -> ");
     //Serial.println(minutos_s.length());
     if (minutos_s.length() == 2) {
-      // Prepare the character array (the buffer) 
-      char char_array[minutos_s.length()];
-      // Copy it over 
-      minutos_s.toCharArray(char_array, minutos_s.length());
+      String n1 = minutos_s.substring(0,1);
+      String n2 = minutos_s.substring(1,2);
       /*
-      Serial.print("char minutos -> ");
-      Serial.print(char_array[0]);
+      Serial.print("string segundos n1 n2 -> ");
+      Serial.print(n1);
       Serial.print(" ");
-      Serial.println(char_array[0]);*/
-      dig.digit[0] = String(char_array[0]).toInt();
-      dig.digit[1] = String(char_array[1]).toInt();
+      Serial.print(n2);*/
+      dig.digit[0] = n1.toInt();
+      dig.digit[1] = n2.toInt();
     } else if (minutos_s.length() == 1) {
       //split      
       dig.digit[1] = minutos_s.toInt();
@@ -406,7 +417,7 @@ struct struct_digits IntToDigits(int n){
   return dig;
 }
 
-// IMPRIMER LOS 4 DIGITOS EN EL DISPLAY 
+// IMPRIMER LOS 4 DIGITOS EN EL DISPLAY
 void PrintNumber(int n, int time) {
   struct struct_digits dig;
   dig = IntToDigits(n); // convierte el numero a un digito
@@ -428,23 +439,30 @@ void PrintNumber(int n, int time) {
     }
   }
 }
-
+ 
+// TEMPORIZADOR
 bool Countdown(int n, int del){
   for (int q=n; q>0; q--){ // for donde disminuye el valor del conteo (Timer)
     Serial.print("countdown -> "); Serial.println(q);
     PrintNumber(q,del); // se muestra el valor en el display
+    Serial.print("button 2 respones -> "); Serial.println(digitalRead(button2));
     if (digitalRead(button2)==LOW) { // BOTON 2 de reset
       return false;
     }
   }
-  PrintNumber(0,0); // el display se settea en 0 y suena la cancion
-  for (int i=0;i<203;i++){ //203 is the total number of music notes in the song
-    int wait = duration[i] * songspeed;
-    tone(buzzer,notes[i],wait); //tone(pin,frequency,duration)
-    delay(wait);}     
   return true;
 }
 
+// CONVERSION SEÑAL DIMMER -> MINUTOS
+void setTotalTime(int dimmer_val) {
+  if (dimmer_val == 0) {
+    total_time = 1;  
+  } else {
+    total_time = ceil(((float)dimmer_val/1023)*45);  
+  }
+}
+
+// FUNCION DE RESET
 void reset() {
   int m, zeros, d, pressed3 = 0, pressed4 = 0; // valores de 
   m=countdown_time;
@@ -454,64 +472,76 @@ void reset() {
   dig = IntToDigits(countdown_time); // conviente el tiempo de conteo (124 default) en un digito
   dig2 = IntToDigits(rest_time);
 
+  bool ajuste_duracion = false;
+
   while (digitalRead(button1)==HIGH) { // BOTON 1 -> empezar timer
     // RESET FUNCION ---------------------------\*
-    delay(2000);
-    for (int j=0; j<4; j++) { // SE RESETEAN LOS VALORES
-      SwitchDigit(j);
-      lightNumber(dig.digit[j]);
-      delay(5);
-    }
-    delay(2000);
-    for (int j=0; j<4; j++) { // SE RESETEAN LOS VALORES
-      SwitchDigit(j);
-      lightNumber(dig2.digit[j]);
-      delay(5);
-    }
+    PrintNumber(countdown_time, 1000);
+    PrintNumber(rest_time, 1000);
     // FIN RESET FUNCION ----------------------/*
     // INCIO DEL ESPACIO DEL DIMMER
-    if (digitalRead(button3)==LOW) {  // BOTON 3 -> incrementar el tiempo
-      if (pressed3 == 0 || pressed3 > 30) {
-        if (countdown_time > 0) {
-          countdown_time -= 1 ;
-        }
-        dig = IntToDigits(countdown_time);
-      } 
-      pressed3 += 1;
-    }  else if (digitalRead(button4)==LOW) {  // BOTON 4 -> decrementar el tiempo
-      if (pressed4 == 0 || pressed4 > 30) {
-        if (countdown_time <9999) {
-          countdown_time += 1 ;
-        }
-        dig = IntToDigits(countdown_time);
-      } 
-      pressed4 += 1;
+    if (digitalRead(button3)==LOW) {
+      ajuste_duracion = true;
+      delay(2000);
+    }
+    while (ajuste_duracion) {
+      int duracion_total_s = total_time*60;
+      PrintNumber(duracion_total_s, 500);
+      int value = analogRead(dimmer);
+      setTotalTime(value);
+      if (digitalRead(button3)==LOW) {
+        ajuste_duracion = false;
+      }
     }
     // FIN DEL ESPACIO DEL DIMMER
-    if (digitalRead(button3)==HIGH) { // BOTON 3 -> INCREMENTAR
-      pressed3=0;
-    }
-    if (digitalRead(button4)==HIGH) { // BOTON 4 -> DECREMENTAR
-      pressed4=0;
-    }
   }
 }
-
 
 void loop(){
   //SwitchDigit(0);
   //lightNumber(2);
-  
-  reset();
-  while (!Countdown(countdown_time,962)) {
+  inicio:
     reset();
-  }
-  while (digitalRead(button2)==1){};
+    //PRIMER POMODORO
+    startWorkingAlarm();
+    while (!Countdown(countdown_time,962)) {
+      reset();
+    }
+    stopWorkingAlarm();
+    while (!Countdown(rest_time,962)) {
+      goto inicio;
+    }
+    //SEGUNDO POMODORO
+    startWorkingAlarm();
+    while (!Countdown(countdown_time,962)) {
+      goto inicio;
+    }
+    stopWorkingAlarm();
+    while (!Countdown(rest_time,962)) {
+      goto inicio;
+    }
+    //TERCER POMODORO
+    startWorkingAlarm();
+    while (!Countdown(countdown_time,962)) {
+      goto inicio;
+    }
+    stopWorkingAlarm();
+    while (!Countdown(rest_time,962)) {
+      goto inicio;
+    }
+    //CUARTO POMODORO
+    startWorkingAlarm();
+    while (!Countdown(countdown_time,962)) {
+      goto inicio;
+    }
+    finishAlarm();
+    while (digitalRead(button2)==1){};
 }
+
 /*
   BOTON 1  -> START
   BOTON 2  -> RESET
-  BOTON 3  -> INC
+  BOTON 3  -> AJUSTE TIEMPO TOTAL
   BOTON 4  -> 
 */
  
